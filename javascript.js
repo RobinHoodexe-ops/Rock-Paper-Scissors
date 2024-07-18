3a3cd5c3d261ac67f3a7374ac71acd077f4e70f8
Dick greyson <flowiii@protonmail.com>
2024-07-18 13:42:31 -0400

    JavaScript logic for the game

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
 
 
 const options = ["rock", "paper", "scissors"];
-
+let humanScore = 0;
+let computerScore = 0;
 
 
 function getRandomChoice(){
@@ -26,5 +27,47 @@ function getRandomChoice(){
 
 let compChoice = getComputerChoice(getRandomChoice,options);
 
+ function getHumanChoice(){
 
+    let hChoice =  prompt("rock,paper,scissors");
+    return hChoice;
+ }
+
+ hChoice = getHumanChoice();
+
+function playRound(humanChoice,computerChoice){
+
+    humanChoice = humanChoice.toLowerCase();
+    if(humanChoice == computerChoice){
+        return "its a tie";
+    }
+    if(humanChoice == "rock" && computerChoice == "paper"){
+        computerScore += 1;
+        return "you lose";
+    }
+    if(humanChoice == "rock" && computerChoice == "scissors"){
+        humanScore += 1;
+        return "you win";
+    }
+    if(humanChoice == "paper" && computerChoice == "rock"){
+        humanScore += 1;
+        return "you win";
+    }
+    if(humanChoice == "paper" && computerChoice == "scissors"){
+        computerScore += 1;
+        return "you lose";
+    }
+    
+}
+
+let game = playRound(hChoice,compChoice);
+
+
+console.log(hChoice);
 console.log(compChoice);
+
+
+
+console.log(game);
+console.log("your score",humanScore);
+console.log("computer score",computerScore);
